3f79e94587dc96da9fdb76fd3a13438d0a4bc8bc
jianding9 <jianding9@iflytek.com>
2020-11-18 17:01:13 +0800

    eslint相关

--- eslint-demo/.eslintrc.js
+++ eslint-demo/.eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-      "browser": true,
-      "es6": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": ["airbnb", "plugin:prettier/recommended"],
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+  extends: ['plugin:react/recommended"', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-  },
-  "rules": {
-    "prettier/prettier": "error",
-    "no-unused-vars": "off",
-    "prettier/prettier": "error",
-  }
+  plugins: ['react'],
+  rules: {},
 };
