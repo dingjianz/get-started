e740a4fda902420a9fc96b1eed71fbc3d6ebca46
dingjianz <573828395@qq.com>
2020-12-16 22:35:29 +0800

    xuexi

--- next_promote/src/pages/getServerSideProps/index.jsx
+++ next_promote/src/pages/getServerSideProps/index.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
+import Link from 'next/link';
 
-const GetServerSidePropsDemo = () => {
-  return <div>\asxcc</div>;
+const GetServerSidePropsDemo = (props) => {
+  const {
+    result: { data },
+  } = props;
+  console.log('data', data);
+  return (
+    <div>
+      {data.map((item) => {
+        return (
+          <Link href={`/songDetail/${item.songId}`} key={item.songStringId}>
+            <a>{item.songName}</a>
+          </Link>
+        );
+      })}
+      <style jsx>
+        {`
+          a {
+            display: block;
+          }
+        `}
+      </style>
+    </div>
+  );
 };
 
 export const getServerSideProps = async () => {
-  const r = await fetch('http://example.com/movies.json')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      console.log(myJson);
-    });
-  console.log(r);
+  // const res = await fetch('http://localhost:10087/api/list');
+  // const data = await res.json();
   return {
-    props: { r },
+    // props: data,
+    props: {
+      result: {
+        code: 0,
+        flag: true,
+        data: [
+          { songStringId: '1', songId: 1, songName: '青花瓷' },
+          { songStringId: '2', songId: 2, songName: '菊花台' },
+        ],
+      },
+    },
   };
 };
 
---
+++ next_promote/src/pages/songDetail/[id].jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SongDetail = () => {
+  return <div>123</div>;
+};
+
+export default SongDetail;
