ab7e089c60825466421254435837e2c35f5c93ea
dingjianz <573828395@qq.com>
2020-12-17 22:41:29 +0800

    study

--- next_promote/src/pages/api/hello.jsx
+++ next_promote/src/pages/api/hello.jsx
@@ -1,11 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  console.log(req.method);
-  console.log(req.query);
-  console.log(req.cookies);
-
+  // console.log(req.method);
+  // console.log(req.query);
+  // console.log(req.cookies);
+  if (req.method === 'GET') {
+    res.status(403);
+    res.end('internet Error');
+    return;
+  }
+  console.log(req.body); // 服务端才能看到 === 本地终端能看到
   res.setHeader('content-type', 'application/json;charset=utf-8');
-  res.statusCode = 200;
+  // res.statusCode = 200;
   res.json({ name: 'John Doe' });
 };
--- next_promote/src/pages/blog/index.jsx
+++ next_promote/src/pages/blog/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 import React, { Component } from 'react';
 import Head from 'next/head';
 import styles from './index.module.scss';
@@ -5,6 +6,23 @@ import styles from './index.module.scss';
 class Blog extends Component {
   state = {
     name: 'jianding9',
+  };
+
+  inputEl = null;
+
+  sendData = async () => {
+    const val = this.inputEl.value;
+    const r = await fetch('http://localhost:10087/api/hello', {
+      body: JSON.stringify({
+        value: val,
+      }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await r.json();
+    console.log('data', data);
   };
 
   render() {
@@ -17,6 +35,11 @@ class Blog extends Component {
         <h1 className={styles.test}>我是blog页面 --- {name}</h1>
         <img className={styles.img_logo} src="/logo.jpg" alt="" />
         <div className={styles.logowrap} />
+        <hr />
+        <input type="text" ref={(el) => (this.inputEl = el)} />
+        <button type="button" onClick={this.sendData}>
+          点击发送 /api/hello
+        </button>
       </div>
     );
   }
