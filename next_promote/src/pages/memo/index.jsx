8f20d563eba430f80e0c0d71a6ec0566ef76535a
jianding9 <jianding9@iflytek.com>
2021-03-12 12:58:20 +0800

    feat(hooks):memo

--- next_promote/src/pages/memo/index.jsx
+++ next_promote/src/pages/memo/index.jsx
@@ -3,6 +3,8 @@
  * @author jianding9
  * @dateTime 20210312
  * @description 在 Function Component 的使用中， React 贴心的提供了 React.memo 这个 HOC（高阶组件），与 PureComponent 很相似，但是是专门给 Function Component 提供的，对 Class Component 并不适用。但是相比于 PureComponent ，React.memo() 可以支持指定一个参数，可以相当于 shouldComponentUpdate 的作用，因此 React.memo() 相对于 PureComponent 来说，用法更加方便。
+ * http://www.ptbird.cn/react-hook-useMemo-purerender.html
+ * https://zh-hans.reactjs.org/docs/react-api.html#reactmemo
  */
 /* eslint-disable */
 import React, {  useState } from 'react';
