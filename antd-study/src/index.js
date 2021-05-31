3ead50d45f00eb638c2f3c0a00a3def98a4fc890
dingjianz <573828395@qq.com>
2021-05-31 23:16:46 +0800

    immutable

--- antd-study/src/index.js
+++ antd-study/src/index.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { render } from 'react-dom'
-import zhCN from 'antd/es/locale/zh_CN'
-import { ConfigProvider } from 'antd'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import { render } from 'react-dom';
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import { mainRouter } from './routes';
+import store from './store';
 
-import './index.less'
-
-import { mainRouter } from './routes'
-import store from './store'
-import { Provider } from 'react-redux'
+import './index.less';
 
 render(
   <Provider store={store}>
     <ConfigProvider locale={zhCN}>
       <Router>
-          <Switch>
-            <Route path="/admin" render={rootProps => {
+        <Switch>
+          <Route
+            path="/admin"
+            render={(rootProps) => {
               // 权限：需要登录才能访问 /admin 本次在App组件里面做权限判断
               // return store.getState().userReducer.isLogin ?  <App {...rootProps} /> : <Redirect to='/login' />
-              return <App {...rootProps} />
-            }} />
-            {
-              mainRouter.map(route => {
-                return <Route key={route.pathname} exact={route.exact} path={route.pathname} component={route.component} />
-              })
-            }
-            <Redirect to="/admin" from="/" exact />
-            <Redirect to="/404" />
-          </Switch>
+              return <App {...rootProps} />;
+            }}
+          />
+          {mainRouter.map((route) => {
+            return (
+              <Route
+                key={route.pathname}
+                exact={route.exact}
+                path={route.pathname}
+                component={route.component}
+              />
+            );
+          })}
+          <Redirect to="/admin" from="/" exact />
+          <Redirect to="/404" />
+        </Switch>
       </Router>
-  </ConfigProvider>
+    </ConfigProvider>
   </Provider>,
   document.getElementById('root')
-)
+);
