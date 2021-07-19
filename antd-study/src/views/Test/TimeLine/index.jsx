de8dff837c7e97181dc39965894951320fcab8b6
jianding9 <jianding9@iflytek.com>
2021-07-19 13:47:32 +0800

    1

--- antd-study/src/views/Test/TimeLine/index.jsx
+++ antd-study/src/views/Test/TimeLine/index.jsx
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import './index.scss'
+import './index.scss';
 
-class TimeLine extends Component {
-  
-  render() {
-    return (
-      <div className="time-line">
-      <button onClick={() => {
-        this.setState({
-          isActive: true
-        })
-      }}>点击</button>
-        <ul>
-          <li>
-            <div className="name">
-              <div className="inner">大赛报名</div>
-            </div>
-            <div className="foot"></div>
-          </li>
-          <li>
-            <div className="name">
-              <div className="inner">A.I.开发者大赛&A.I.辩世界</div>
-            </div>
-            <div className="foot"></div>
-          </li>
-          <li>
-            <div className="name">
-              <div className="inner">ENJOY 1024</div>
-            </div>
-            <div className="foot"></div>
-          </li>
-        </ul>
+const TimeLine = () => {
+  return (
+    <div className="time-line">
+      <div className="text-wrap">
+        <h2 className="text">背景</h2>
       </div>
-    )
-  }
-}
+      <blockquote>
+        <p>⚠️本文为掘金社区首发签约文章，未获授权禁止转载</p>
+      </blockquote>
+      <ul>
+        <li>
+          <div className="name">
+            <div className="inner">大赛报名</div>
+          </div>
+          <div className="foot" />
+        </li>
+        <li>
+          <div className="name">
+            <div className="inner">A.I.开发者大赛&A.I.辩世界</div>
+          </div>
+          <div className="foot" />
+        </li>
+        <li>
+          <div className="name">
+            <div className="inner">ENJOY 1024</div>
+          </div>
+          <div className="foot" />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default TimeLine
+export default TimeLine;
