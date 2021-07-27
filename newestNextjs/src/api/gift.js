62ef7cc9b49dd050d426e4b25510ea1d8a5c0367
jianding9 <jianding9@iflytek.com>
2021-07-27 17:40:59 +0800

    1

--- newestNextjs/src/api/gift.js
+++
@@ -1,9 +0,0 @@
-import http from '@utils/http';
-
-/**
- * @param params
- */
-// eslint-disable-next-line import/prefer-default-export
-export const getGiftInfo = () => {
-  return http.get('/cms/page-data/process-new');
-};
