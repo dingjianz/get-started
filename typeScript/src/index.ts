d7ff0c9bcbaa1700abba127f7810051c1d526161
jianding9 <jianding9@iflytek.com>
2021-09-23 17:35:30 +0800

    typescript

---
+++ typeScript/src/index.ts
@@ -0,0 +1,5 @@
+import { hi } from "./m1";
+
+console.log(hi)
+console.log("hello");
+console.log(123);
---
+++ typeScript/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            //指定加载器
+            loader: "babel-loader",
+            options: {
+              // 设置预定义的环境
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: "88",
+                      ie: "11",
+                    },
+                    // 指定coreJs的版本
+                    corejs: "3",
+                    // 使用coreJs的方式usage，表示按需加载
+                    useBuiltIns: "usage",
+                  },
+                ],
+              ],
+            },
+          },
+          "ts-loader",
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: "这是自定义的title",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+};
