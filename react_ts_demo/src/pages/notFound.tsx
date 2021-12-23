0dd6c604c3886f406fb199b0ee08d7b2b661fc74
jianding9 <jianding9@iflytek.com>
2021-12-23 17:30:58 +0800

    context学习

--- react_ts_demo/src/pages/notFound.tsx
+++ react_ts_demo/src/pages/notFound.tsx
@@ -1,10 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
+import ThemeContext from "api/theme";
 
 interface Props {
   [propName: string]: any;
 }
 
 const NotFoundDemo: FC<Props> = (props) => {
+  const context = useContext(ThemeContext);
+  console.log("useContext:::", context);
   return <div>直接就404了啊</div>;
 };
 
